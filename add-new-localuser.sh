8f732c96f8477ae889bcc403e5eca0abd19a8b1d
Mostafa Aborizka <mostafaaborizka1@gmail.com>
2025-10-05 18:27:12 +0300

    Add files via upload

--- add-new-localuser.sh
+++ add-new-localuser.sh
@@ -5,7 +5,7 @@
 #make sure the script is excuted with superuser privileges.
 if [[ ${UID} -ne 0 ]]
 then 
-   echo "please run with sudo or as root"
+   echo "please run with sudo or as root" >&2
    exit 1
 fi 
  
@@ -13,8 +13,8 @@ fi
 #make sure the user provided at least one argument.
 if [[ ${#} -eq 0 ]] 
 then
-   echo "please provide a username and (optional) a real name" 
-   echo "Usage: ${0} USER_NAME [COMMENT]..."
+   echo "please provide a username and (optional) a real name" >&2 
+   echo "Usage: ${0} USER_NAME [COMMENT]..." >&2
    exit 1
 fi 
 
@@ -27,26 +27,26 @@ COMMENT="${@}"
 PASSWORD=$(date +%s%N | sha256sum | head -c16)
 
 #create user with the arguments provided.
-useradd -c "${COMMENT}" -m ${USER_NAME} 
+useradd -c "${COMMENT}" -m ${USER_NAME} > /dev/null
 
 #check to see if the useradd command succeeded.
 if [[ "${?}" -ne 0 ]]
 then
-   echo "Failed to create a new account"
+   echo "Failed to create a new account" >&2
    exit 1
 fi 
 
 #set the password.
-echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+echo ${PASSWORD} | passwd --stdin ${USER_NAME} > /dev/null
 #check to see if the passwd command succeeded.
 if [[ "${?}" -ne 0 ]] 
 then 
-   echo "Failed to set password"
+   echo "Failed to set password" >&2
    exit 1
 fi
 
 #force password change on first login.
-passwd -e ${USER_NAME}
+passwd -e ${USER_NAME} > /dev/null
 
 #Display username, password and the host where the account was created.
 echo "Username: ${USER_NAME}"
